626ec761564c8bf6e8af2d5861f3a97981c5d31c
Kyle Calder <kylebcalder03@gmail.com>
2021-04-22 23:27:51 -0600

    Started working on login page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="display-4 mb-5 text-center">Welcome to A-political!</h1>
+    <router-view />
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container login">
+    <div class="row justify-content-center">
+      <div class="col">
+        <blockquote class="blockquote">
+          <h2 class="mb-0">Sign In</h2>
+          <footer class="blockquote-footer">Or create and account</footer>
+        </blockquote>
+        <div class="input-group d-flex justify-content-center">
+          <!--h1>Sign In</h1>
+        <router-link to="/NewUser" active-class="active" tag="li">
+          <h5>Or <a>create an account</a></h5>
+        </router-link-->
+          <form class="d-flex flex-column col-4">
+            <div class="form-group">
+              <label for="exampleInputEmail1" class="text-left"
+                >Email address</label
+              >
+              <input
+                type="email"
+                class="form-control"
+                v-model="input.email"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <!--small id="emailHelp" class="form-text text-muted"
+              >We'll never share your email with anyone else.</small
+            --></div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                v-model="input.password"
+                placeholder="Password"
+              />
+            </div>
+            <div class="d-flex justify-content-end">
+              <button type="submit" class="btn btn-primary">Sign In</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
